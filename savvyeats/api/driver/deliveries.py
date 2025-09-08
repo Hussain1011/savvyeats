c4ab2ed8039b85241074c0470dbe88cb76223c03
Muhammad  Nouman <muhammadnouman@MacBook-Pro.local>
2025-09-08 17:36:57 +0300

    Delivery Plan

--- savvyeats/api/driver/deliveries.py
+++ savvyeats/api/driver/deliveries.py
@@ -1,18 +1,22 @@
 import frappe
 import random
-from frappe.utils import add_to_date, now_datetime, escape_html
+from frappe.utils import add_to_date, now_datetime, escape_html, getdate
 from savvyeats.api.user import send_error_response, send_success_response
 
 @frappe.whitelist(methods=["GET"])
 def get_delivery_trips(driver_id):
-	delivery_trips_list = frappe.get_all("Delivery Trip", filters={"driver": driver_id, "docstatus": 1, "status": "Scheduled", "start_time": })
+	today = getdate()
+	start_time = "{0} 00:00:00".format(str(today))
+	end_time = "{0} 23:59:59".format(str(today))
+	delivery_trips_list = frappe.get_all("Delivery Trip", filters={"driver": driver_id, "docstatus": 1, "status": "Scheduled", "departure_time": [">=", start_time], "departure_time": ["<=", end_time]})
 	if not delivery_trips_list:
 		return send_success_response("", "", {"delivery_trips": []})
 	delivery_trips = []
 	for d in delivery_trips_list:
 		doc = frappe.get_doc("Delivery Trip", d.name, ignore_permissions=True).as_dict()
 		for ds in doc.delivery_stops:
-			ds.delivery_note_doc = frappe.get_doc("Delivery Note", ds.delivery_note, ignore_permissions=True)
+			ds.address_doc = frappe.get_doc("Address", ds.address, ignore_permissions=True)
+			ds.address_doc = frappe.get_doc("Address", ds.address, ignore_permissions=True)
 
 		delivery_trips.append(doc)
 
@@ -28,7 +32,7 @@ def start_delivery(driver_id):
 	for d in delivery_trips_list:
 		doc = frappe.get_doc("Delivery Trip", d.name, ignore_permissions=True).as_dict()
 		for ds in doc.delivery_stops:
-			ds.delivery_note_doc = frappe.get_doc("Delivery Note", ds.delivery_note, ignore_permissions=True)
+			ds.address_doc = frappe.get_doc("Address", ds.address, ignore_permissions=True)
 
 		delivery_trips.append(doc)
 
--- savvyeats/overrides/delivery_trip_savvyeats.py
+++ savvyeats/overrides/delivery_trip_savvyeats.py
@@ -1,5 +1,7 @@
 import frappe
+from frappe import _
 from erpnext.stock.doctype.delivery_trip.delivery_trip import DeliveryTrip
+from frappe.utils import cint, get_datetime, get_link_to_form
 
 class DeliveryTripOverride(DeliveryTrip):
 	def form_route_list(self, optimize):
@@ -44,5 +46,48 @@ class DeliveryTripOverride(DeliveryTrip):
 
 
 		return route_list
+
+	def update_delivery_notes(self, delete=False):
+		"""
+		Update all connected Delivery Notes with Delivery Trip details
+		(Driver, Vehicle, etc.). If `delete` is `True`, then details
+		are removed.
+
+		Args:
+		        delete (bool, optional): Defaults to `False`. `True` if driver details need to be emptied, else `False`.
+		"""
+
+		delivery_notes = list(set(stop.delivery_note for stop in self.delivery_stops if stop.delivery_note))
+
+		update_fields = {
+			"driver": self.driver,
+			"driver_name": self.driver_name,
+			"vehicle_no": self.vehicle,
+			"lr_no": self.name,
+			"lr_date": self.departure_time,
+			"delivery_trip": self.name,
+			"delivery_status": "Scheduled"
+		}
+
+		for delivery_note in self.delivery_stops:
+			if not delivery_note.delivery_note:
+				continue
+			note_doc = frappe.get_doc("Delivery Note", delivery_note.delivery_note)
+			update_fields = {
+				"latitude": delivery_note.latitude,
+				"longitude": delivery_note.longitude,
+				"distance": delivery_note.distance,
+				"distance_uom": delivery_note.uom,
+				"estimated_arrival": delivery_note.estimated_arrival
+			}
+			for field, value in update_fields.items():
+				value = None if delete else value
+				setattr(note_doc, field, value)
+
+			note_doc.flags.ignore_validate_update_after_submit = True
+			note_doc.save()
+
+		delivery_notes = [get_link_to_form("Delivery Note", note) for note in delivery_notes]
+		frappe.msgprint(_("Delivery Notes {0} updated").format(", ".join(delivery_notes)))
 
 		
