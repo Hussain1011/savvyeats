f96d228284237a977d9eaf3812ee624ded393eba
Muhammad  Nouman <muhammadnouman@MacBook-Pro.local>
2025-09-09 15:47:52 +0300

    Hook to on update after submit sales order

--- savvyeats/hooks.py
+++ savvyeats/hooks.py
@@ -156,7 +156,8 @@ doc_events = {
 	},
 	"Sales Order": {
 		"validate": "savvyeats.custom.sales_order_savvyeats.validate",
-		"before_submit": "savvyeats.custom.sales_order_savvyeats.before_submit"
+		"before_submit": "savvyeats.custom.sales_order_savvyeats.before_submit",
+		"on_update_after_submit": "savvyeats.custom.sales_order_savvyeats.before_submit"
 	},
 	"Delivery Note": {
 		"validate": "savvyeats.custom.delivery_note_savvyeats.validate",
